cba5d785756e019af3a0add504d7aa763da5d6b9
niko <143855663+nikobarros@users.noreply.github.com>
2025-10-16 10:57:24 -0300

    Implement dataset download and extraction
    Added functionality to download and extract the credit card fraud dataset if it doesn't exist.

--- Proyecto2/d.py
+++ Proyecto2/d.py
@@ -1,8 +1,23 @@
 #imports
 from pathlib import Path
 import pandas as pd
+import os
+from zipfile import ZipFile
 
+# Si no existe el dataset, lo descarga
+os.mkdir("dataset") if not os.path.exists("dataset") else None
+if not os.path.exists("creditcard.csv"):
+    os.system("kaggle datasets download -d mlg-ulb/creditcardfraud -p ./dataset")
 
+    # Descomprimir
+    with ZipFile("./dataset/creditcardfraud.zip", "r") as zip_ref:
+        zip_ref.extractall("./dataset")
+    
+    rmfile = Path("dataset/creditcardfraud.zip")
+    if rmfile.exists():
+        rmfile.unlink()
+        
+print("Dataset descargado correctamente")
 
 csv_path = Path("dataset/creditcard.csv")
 
